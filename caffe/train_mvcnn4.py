b26fede553290b2709164565871db1cc3f07734d
Kamel Abdelouahab <kamel_eddine.abdelouahab@uca.fr>
2018-04-24 13:55:00 +0200

    Added prototxt of a 4 view model

---
+++ caffe/train_mvcnn4.py
@@ -0,0 +1,11 @@
+import numpy as np
+import sys
+import caffe
+import scipy.io as sio
+import h5py
+import os
+caffe.set_mode_gpu()
+caffe.set_device(0)
+solver = caffe.SGDSolver('mvcnn4_solver.prototxt')
+solver.net.copy_from('alexnet_ft.caffemodel')
+solver.solve()
