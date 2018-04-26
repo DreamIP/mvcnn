98ab5fd868909d7aea698e5f0f247616801bfc9b
Kamel Abdelouahab <kamel_eddine.abdelouahab@uca.fr>
2018-04-26 12:14:33 +0200

    Added a two view netpa model

---
+++ topology/train_mvnetpa2.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+import numpy as np
+import sys
+import caffe
+import scipy.io as sio
+import h5py
+import os
+caffe.set_mode_gpu()
+caffe.set_device(0)
+solver = caffe.SGDSolver('mvnetpa2_solver.prototxt')
+solver.net.copy_from('netpa.caffemodel')
+solver.solve()
