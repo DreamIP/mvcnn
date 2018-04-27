2686614086711912775ca7ed76358b40f779e362
Kamel Abdelouahab <kamel_eddine.abdelouahab@uca.fr>
2018-04-27 16:04:42 +0200

    Experimenting where to split mvcnns

---
+++ split_position/eval_acc.py
@@ -0,0 +1,31 @@
+import os
+import numpy as np
+os.environ['GLOG_minloglevel'] = '2'
+import caffe
+
+def EvalCaffeAcc(proto_file, weight_file, num_iter):
+    net = caffe.Net(proto_file,weight_file,caffe.TEST)
+    acc = 0
+    for itter in range(num_iter):
+        acc+=net.forward()['accuracy']
+    acc = acc / num_iter
+    print(weight_file + ": " + str(100*acc))
+
+def main():
+    num_iter = 2000
+    ## ENABLE GPU MODE
+    caffe.set_mode_gpu()
+    caffe.set_device(0)
+    nb_views = ['2','3','4','11','12']                 # number of views
+    nb_views = '3'
+    for nv in nb_views:
+        proto_file = 'mvcnn' + nv + '.prototxt'          # File name of proto
+        for state_itter in range(100,2100,100):      # loop accross snapshots
+            weight_file = '../caffemodel/mvcnn' + nv + '.caffemodel'
+            print(weight_file)
+            if(os.path.isfile(weight_file) and os.path.isfile(proto_file)) :
+                EvalCaffeAcc(proto_file,weight_file,num_iter)
+if __name__ == '__main__':
+
+    main()
+    os.environ['GLOG_minloglevel'] = '0'
